676474685d6efe6b010cfc638cade8c33f3ed179
Jose A. Hernando <hernando.joseangel@gmail.com>
2020-11-10 17:07:34 +0100

    utils.py

---
+++ hipy/utils.py
@@ -0,0 +1,34 @@
+import numpy             as np
+#import pandas            as pd
+#import tables            as tb
+#import matplotlib.pyplot as plt
+#from scipy               import optimize
+#import invisible_cities.core.fit_functions as fitf
+
+
+#--- histograming
+
+def remove_nan(vals : np.array) -> np.array:
+     return vals[~np.isnan(vals)]
+
+
+def select_in_range(vals : np.array, range : tuple = None) -> np.array(bool):
+    if (range is None): return np.ones(len(vals))
+    sel = (vals >= range[0]) & (vals < range[1])
+    return sel
+
+
+def stats(vals : np.array, range : tuple = None):
+    vals = remove_nan(vals)
+    sel  = select_in_range(vals, range)
+    vv = vals[sel]
+    mean, std, evts, oevts = np.mean(vv), np.std(vv), len(vv), len(vals) - len(vv)
+    return evts, mean, std, oevts
+
+
+def str_stats(vals, range = None, formate = '6.2f'):
+    evts, mean, std, ovts = stats(vals, range)
+    s  = 'events '+str(evts)+'\n'
+    s += (('mean {0:'+formate+'}').format(mean))+'\n'
+    s += (('std  {0:'+formate+'}').format(std))
+    return s
