d1c3adf67720c7c90ac7e948467b4be172074bba
Jose A. Hernando <hernando.joseangel@gmail.com>
2020-11-19 16:06:06 +0100

    display

--- bes/display.py
+++ bes/display.py
@@ -26,9 +26,19 @@ def track(x, y, z, ene, scale = 10., title = '', cmap = 'magma'):
 
 
 def event(x, y, z, ene, scale = 10., rscale = 9., chamber = False, **kargs):
+    """ Draw an event with hits x, y, z, ene
+    inputs:
+        x   : np.array, x-hit positions
+        y   : np.array, y-hit positions
+        z   : np.array, z-hit positions
+        ene : np.array, energy or intensity of the hits
+        scale  : float, scale factor of the markers
+        rscale : float, scale factor of the size with energy/intensity
+        chamber: bool (false), in NEW x,y,z frame
+    """
 
-    if (not 'alpha'  in kargs.keys()): kargs['alpha']  = 0.2
-    if (not 'cmap'   in kargs.keys()): kargs['cmap']   = 'magma'
+    if (not 'alpha'  in kargs.keys()): kargs['alpha']  = 0.5
+    if (not 'cmap'   in kargs.keys()): kargs['cmap']   = 'Greys'
     if (not 'marker' in kargs.keys()): kargs['marker'] = 's'
 
     rene = ut.arscale(ene)
@@ -76,4 +86,71 @@ def event(x, y, z, ene, scale = 10., rscale = 9., chamber = False, **kargs):
         plt.xlim(xysize); plt.ylim(xysize)
     plt.colorbar();
     plt.tight_layout()
+    return
+
+
+def wf(z, e, eraw = None, step = 2.):
+    """ Draw the (e, z) wave-form
+    inputs:
+        z    : np.array, z-hit positions
+        e    : np.array, energy or intensity of the hits
+        eraw : np.array (optional), energy raw of the hits
+        step : float (2), wf-step size
+    """
+
+    bins =  ut.arstep(z, step)
+    nplots = 1 if eraw is None else 3
+    subplot = pltext.canvas(nplots)
+
+    subplot(1)
+    pltext.hist(z, bins, weights = e, alpha = 0.5, stats = False, lw = 2)
+    plt.xlabel('z (mm)'); plt.ylabel('E (keV)')
+
+    if (eraw is None): return
+
+    plt.gca().twinx();
+    pltext.hist(z, bins, weights = eraw, alpha = 0.4, stats = False,
+                lw = 2, color = 'blue')
+    plt.xlabel('z (mm)'); plt.ylabel('E (adc)', c = 'blue')
+    plt.legend();
+
+    subplot(2)
+    pltext.hist(z, bins, weights = e/eraw, stats = False)
+    plt.xlabel('z (mm)'); plt.ylabel('calibration (keV/adc)')
+    plt.tight_layout()
+
+    return
+
+
+def xyspot(x, y, e, eraw = None, step = 10.):
+    """ Draw the (x, y) enery spot
+    inputs:
+        x    : np.array, x-hit positions
+        y    : np.array, y-hit positions
+        e    : np.array, energy or intensity of the hits
+        eraw : np.array (optional), energy raw of the hits (optional)
+        step : float (10), xy-step size
+    """
+
+
+    bins = (ut.arstep(x, step), ut.arstep(y, step))
+
+    nplots = 1 if eraw is None else 3
+    subplot = pltext.canvas(nplots)
+
+    subplot(1)
+    plt.hist2d(x, y, bins, weights = e, cmap = 'Greys');
+    plt.xlabel('x (mm)'); plt.ylabel('y (mm)');
+    cbar = plt.colorbar(); cbar.set_label('Energy (keV)')
+
+    subplot(2)
+    plt.hist2d(x, y, bins, weights = eraw, cmap = 'Greys');
+    plt.xlabel('x (mm)'); plt.ylabel('y (mm)');
+    cbar = plt.colorbar(); cbar.set_label('Energy (adc)')
+
+    subplot(3)
+    plt.hist2d(x, y, bins, weights = e/eraw, cmap = 'Greys');
+    plt.xlabel('x (mm)'); plt.ylabel('y (mm)');
+    cbar = plt.colorbar(); cbar.set_label('calibration factor (keV/adc)')
+
     return
