45cd5d09a8250fa7e3f0f9c1868a13244889f3fa
Jose A. Hernando <hernando.joseangel@gmail.com>
2020-11-19 10:16:07 +0100

    display

---
+++ bes/display.py
@@ -0,0 +1,61 @@
+import numpy             as np
+import pandas            as pd
+import tables            as tb
+import matplotlib.pyplot as plt
+
+import hipy.pltext.py    as pltext
+
+
+from mpl_toolkits.mplot3d    import Axes3D
+
+
+def track(x, y, z, ene, scale = 10., title = '', cmap = 'magma'):
+
+    rene = arscale(ene, scale)
+
+    ax   = plt.subplot(111, projection = '3d')
+    ax.scatter(x, y, z, c = rene, s = rene, alpha = 0.2, cmap = cmap)
+    ax.set_xlabel('X (mm)');
+    ax.set_ylabel('Y (mm)');
+    ax.set_zlabel('Z (mm)');
+    plt.gcf().colorbar();
+    #ax.colorbar()
+
+    return
+
+
+def event(x, y, z, ene, scale = 1., title = '', cmap = 'magma'):
+
+    rene = arscale(ene, scale)
+
+    fig = plt.figure(figsize=(12, 9));
+    #plt.subplots(2
+    ax3D = fig.add_subplot(221, projection='3d')
+    p3d = ax3D.scatter(z, x, y, s = rene, c = rene, alpha = 0.2, marker='o')
+    ax3D.set_xlabel('z (mm)')
+    ax3D.set_ylabel('x (mm)')
+    ax3D.set_zlabel('y (mm)')
+    plt.title(title)
+
+    plt.subplot(2, 2, 2)
+    plt.scatter(x, z, c = ene, s = rene, alpha = 0.2, cmap = cmap)
+    ax = plt.gca()
+    ax.set_xlabel('x (mm)')
+    ax.set_ylabel('z (mm)')
+    plt.colorbar();
+
+    plt.subplot(2, 2, 3)
+    plt.scatter(z, y, c=ene, s= rene, alpha = 0.2, cmap = cmap)
+    ax = plt.gca()
+    ax.set_xlabel('z (mm)')
+    ax.set_ylabel('y (mm)')
+    plt.colorbar();
+
+    plt.subplot(2, 2, 4)
+    plt.scatter(x, y, c=ene, s= rene, alpha = 0.2, cmap = cmap)
+    ax = plt.gca()
+    ax.set_xlabel('x (mm)')
+    ax.set_ylabel('y (mm)')
+    plt.colorbar();
+    plt.tight_layout()
+    return
--- hipy/utils.py
+++ hipy/utils.py
@@ -9,16 +9,45 @@ import numpy             as np
 #--- general utilies
 
 def remove_nan(vals : np.array) -> np.array:
-     return vals[~np.isnan(vals)]
+    """ returns the np.array without nan
+    """
+    return vals[~np.isnan(vals)]
 
 
 def in_range(vals : np.array, range : tuple = None) -> np.array(bool):
+    """ returns a np.array(bool) with the elements of val that are in range
+    inputs:
+        vals : np.array
+        range: tuple(x0, x1)
+    returns
+        np.array(bool) where True/False indicates if the elements of vals is in rage
+    """
     if (range is None): return vals >= np.min(vals)
     sel = (vals >= range[0]) & (vals < range[1])
     return sel
 
-def centers(xs):
+def centers(xs : np.array) -> np.array:
+    """ returns the center between the participn
+    inputs:
+        xs: np.array
+    returns:
+        np.array with the centers of xs (dimension len(xs)-1)
+    """
     return 0.5* ( xs[1: ] + xs[: -1])
+
+
+def arscale(x, scale = 1.):
+    """ return an arrey between [0., 1.]
+    inputs:
+        x    : np.array,
+        scale: float (1.)
+    returns:
+        np.arry with scaled balues, [0, scale]
+    """
+    xmin, xmax = np.min(x), np.max(x)
+    rx = scale * (x - xmin)/(xmax - xmin)
+    return rx
+
 
 def stats(vals : np.array, range : tuple = None):
     vals = remove_nan(vals)
@@ -30,7 +59,7 @@ def stats(vals : np.array, range : tuple = None):
 
 def str_stats(vals, range = None, formate = '6.2f'):
     evts, mean, std, ovts = stats(vals, range)
-    s  = 'events '+str(evts)+'\n'
+    s  = 'entries '+str(evts)+'\n'
     s += (('mean {0:'+formate+'}').format(mean))+'\n'
     s += (('std  {0:'+formate+'}').format(std))
     return s
