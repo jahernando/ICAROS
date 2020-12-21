db5e503776c396223f410d78407d767a548c5093
Jose A. Hernando <hernando.joseangel@gmail.com>
2020-12-21 11:45:20 +0100

    chits

--- bes/chits.py
+++ bes/chits.py
@@ -110,7 +110,7 @@ def init_hits_summary(nsize = 1):
 def hits_summary(ddhits, q0 = 0., corrfac = None):
     x, y, z, eraw, erec, q, times = get_filter_hits(ddhits, q0)
     nout = np.sum(np.isnan(erec))
-    if (coorfac):
+    if (corrfac is not None):
         cfac   = corrfac(x, y, z, times)
         nout   = np.sum(np.isnan(cfac))
 
